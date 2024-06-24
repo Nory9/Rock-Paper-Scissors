62384e523c2eec1eed5462113ad7d4afa0d7fe2b
Nory9 <noormisk94@gmail.com>
2024-06-24 11:39:11 +0300

    RPS game implementation

---
+++ Rock-Paper-Scissors/Player.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rock_Paper_Scissors
+{
+    internal class Player
+    {
+        private string _name;
+        private int _score;
+
+        public string Name { 
+            get { return _name; }
+            set { _name = value; }
+        }
+
+        public int Score
+        {
+            get { return _score; }
+            set { _score = value; }
+        
+        }
+
+        public int playersMove(string move) {
+          //  string move = "";
+          //  Console.WriteLine("please choose a move : Rock , Paper , Scisseor");
+            //move = Console.ReadLine();
+            string checked_move= validation(move);
+            if (checked_move != "")
+            {
+                if (checked_move == "rock")
+                    return 1;
+                else if (checked_move == "paper")
+                    return 2;
+                else
+                    return 3;
+            }
+            else {
+              
+                return -1;
+            }
+        }
+
+        public string validation(string move) { 
+         if(move.ToLower()=="rock"||move.ToLower()=="paper"||move.ToLower()== "scisseor")
+                return move.ToLower();
+         else 
+                return "";
+
+        }
+    }
+}
---
+++ Rock-Paper-Scissors/RPSGame.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rock_Paper_Scissors
+{
+    internal class RPSGame
+    {
+       
+        public RPSGame() {
+           
+        }
+
+        public void GameFlow() {
+            Console.WriteLine(" Wellcome to our RPS game where you can play with AI lets get started!!\n");
+            for (int i = 1; i <= 3; i++)
+            {
+                Console.WriteLine($" This is round {i}");
+                 round();
+            }
+        }// \n\n  please enter your name: \n");
+
+        public int PlayerMoves(Player player)
+        {
+            Console.WriteLine(" please choose a move : Rock , Paper , Scisseor :\n ");
+            string move = Console.ReadLine();
+            int playedMove = player.playersMove(move);
+            return playedMove;
+        }
+
+        public int AiMove() {
+            Random rnd = new Random();
+            int movee=rnd.Next(1, 4);
+            return movee;
+        }
+
+        public int roundWinner(int playerMove, int aiMove) {
+            if (playerMove == aiMove)
+            {
+                return 0;
+            }
+            else {
+                if ((playerMove == 2 && aiMove == 1) || (playerMove == 1 && aiMove == 3) || (playerMove == 3 && aiMove == 2))
+                {
+                    return 1;
+                }
+                else
+                    return -1;
+
+            }
+        }
+
+        public string Answer(int ans) {
+            string answer="";
+            if (ans == 1)
+                answer = "Rock";
+            else if (ans == 2)
+                answer = "Paper";
+            else if (ans == 3)
+                answer = "Scisseor";
+            return answer;
+        }
+
+        public void round() {
+            Player player = new Player();
+            Console.WriteLine(" please enter your name: ");
+            string name = Console.ReadLine();
+            player.Name = name;
+            int n = 0;
+            int aiScore = 0;
+            while (n != 3)
+            {
+                int ans = PlayerMoves(player);
+                if (ans == -1)
+                {
+                    Console.WriteLine("your entered move is invalid! please try again and make sure your move is either Rock , Paper , Scisseor\n\n");
+                    return;
+                }
+                int ai = AiMove();
+                int winner = roundWinner(ans, ai);
+                if (winner == 1)
+                    player.Score++;
+                else if(winner==-1)
+                    aiScore++;
+                else
+                {
+                    aiScore++;
+                    player.Score++;
+                }
+                string Panswer = Answer(ans);
+                string Aanswer = Answer(ai);
+                Console.WriteLine($" {name} move : {Panswer} \n AI move : {Aanswer} \n\n your socre now : {player.Score} \n AI socre : {aiScore}\n ");
+                n++;
+            }
+            int finalWinner =winner(player.Score,aiScore);
+            if (finalWinner == 1)
+                Console.WriteLine("Congrats! you won :)\n\n");
+            else if (finalWinner == -1)
+                Console.WriteLine("AI won on you! but dont worry you can always play again\n\n");
+            else
+                Console.WriteLine("this round is Draw no  one won!\n\n");
+
+        }
+
+        public int winner(int playerScore,int aiScore) {
+            if (playerScore > aiScore)
+                return 1;
+            else if (playerScore < aiScore)
+                return -1;
+            else
+                return 0;
+           
+        }
+
+    }
+}
